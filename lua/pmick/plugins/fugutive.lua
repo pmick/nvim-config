f6281781baac0b54495ca8a188eebc050f87a597
Patrick Mick <patrick.mick@discordapp.com>
2025-01-12 13:29:12 -0800

    Add ruby to treesitter and update some binds

--- lua/pmick/plugins/fugutive.lua
+++ lua/pmick/plugins/fugutive.lua
@@ -1,5 +1,5 @@
 return {
-    "pope/vim-fugitive",
+    "tpope/vim-fugitive",
     config = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
     end
--- lua/pmick/plugins/treesitter.lua
+++ lua/pmick/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         require 'nvim-treesitter.configs'.setup {
             -- A list of parser names, or "all" (the five listed parsers should always be installed)
-            ensure_installed = { "c", "javascript", "typescript", "lua", "vim", "vimdoc", "query", "starlark", "java", "kotlin", "objc", "swift", "python" },
+            ensure_installed = { "c", "javascript", "typescript", "lua", "vim", "vimdoc", "query", "starlark", "java", "kotlin", "objc", "swift", "python", "ruby" },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
