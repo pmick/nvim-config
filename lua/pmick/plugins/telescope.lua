c7937b91e0883be82765a3dd594337fab8cea08d
Patrick Mick <patrick.mick@discordapp.com>
2025-01-23 10:31:14 -0800

    Add fzf for telescope

--- lua/pmick/plugins/telescope.lua
+++ lua/pmick/plugins/telescope.lua
@@ -1,12 +1,21 @@
 return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    },
     config = function()
         local telescope = require('telescope')
         local builtin = require('telescope.builtin')
 
-        telescope.setup({})
+        telescope.setup({
+            extensions = {
+                fzf = {}
+            }
+        })
+
+        require('telescope').load_extension('fzf')
 
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
